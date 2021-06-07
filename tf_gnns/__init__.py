d97adde495996c5bcfb511462f7673b68934bb5f
Mylonas Charilaos <mylonas.charilaos@gmail.com>
2021-06-07 21:38:57 +0200

    correction in __init__.py

--- tf_gnns/__init__.py
+++ tf_gnns/__init__.py
@@ -6,5 +6,5 @@ from .datastructures  import Graph, GraphTuple, Node, Edge, make_graph_tuple_fro
 from .graphnet_utils  import GraphNet, make_node_mlp, make_edge_mlp, make_keras_simple_agg, make_mlp_graphnet_functions, make_global_mlp
 from .graphnet_utils  import _aggregation_function_factory, make_full_graphnet_functions, make_graph_indep_graphnet_functions, make_graph_to_graph_and_global_functions
 from .lib.gt_ops      import _add_gt, _assign_add_tensor_dict, _concat_tensordicts, _copy_structure, _slice_conc_tensordict, _zero_graph, _zero_graph_tf 
-from .models.graphnet import GraphNetMLP, GraphIndep
+from .models.graphnet import GraphNetMLP, GraphIndep, GNCellMLP
 print("loaded tfgnn lib")
