9ebf7c0dc26a8d72bbac66e370fa078c6673e8d0
Mylonas Charilaos <mylonas.charilaos@gmail.com>
2026-06-17 12:14:54 +0200

    expand graphnet and gt ops coverage

--- tests/test_models_graphnet.py
+++ tests/test_models_graphnet.py
@@ -1,7 +1,7 @@
 import pytest
 import tensorflow as tf
 
-from tf_gnns.models.graphnet import GraphIndep, GraphNetMLP, GraphNetMPNN_MLP
+from tf_gnns.models.graphnet import GNCellMLP, GraphIndep, GraphNetMLP, GraphNetMPNN_MLP
 from tf_gnns.tfgnns_datastructures import GraphTuple
 
 
@@ -117,3 +117,51 @@ def test_graphindep_builds_and_runs_with_and_without_global():
     m_no_global = GraphIndep(units_out=5)
     with pytest.raises(TypeError):
         m_no_global(td_no_global)
+
+
+def test_gncell_mlp_builds_with_global_and_without_global():
+    td_with_global = _make_td(with_global=True)
+    td_no_global = _make_td(with_global=False)
+    td_no_global_no_key = dict(td_no_global)
+    td_no_global_no_key.pop("global_attr")
+
+    with_global = GNCellMLP(gn_mlp_units=8, core_size=4)
+    out_global = with_global(td_with_global)
+    assert out_global["nodes"].shape == (td_with_global["nodes"].shape[0], 4)
+    assert out_global["edges"].shape == (td_with_global["edges"].shape[0], 4)
+    assert out_global["global_attr"].shape == (td_with_global["global_attr"].shape[0], 4)
+
+    no_global = GNCellMLP(gn_mlp_units=8, core_size=5)
+    out_no_global = no_global(td_no_global_no_key)
+    assert out_no_global["nodes"].shape == (td_no_global["nodes"].shape[0], 5)
+    assert out_no_global["edges"].shape == (td_no_global["edges"].shape[0], 5)
+
+
+def test_graphindep_without_global_attr_key_builds_no_global_path():
+    td = _make_td(with_global=False)
+    td.pop("global_attr")
+    model = GraphIndep(units_out=4)
+
+    out = model(td)
+
+    assert out["nodes"].shape == (td["nodes"].shape[0], 4)
+    assert out["edges"].shape == (td["edges"].shape[0], 4)
+
+
+def test_model_html_repr_before_and_after_build():
+    td = _make_td(with_global=True)
+
+    graphnet = GraphNetMLP(units=4, core_steps=1)
+    assert "Layer not yet built" in graphnet._repr_html_()
+    _ = graphnet(td)
+    assert "GN Deterministic path" in graphnet._repr_html_()
+
+    mpnn = GraphNetMPNN_MLP(units=4, core_steps=1)
+    assert "Layer not yet built" in mpnn._repr_html_()
+    _ = mpnn(_make_td(with_global=False))
+    assert "GN Deterministic path" in mpnn._repr_html_()
+
+    indep = GraphIndep(units_out=4)
+    assert "Not built" in indep._repr_html_()
+    _ = indep(td)
+    assert "GraphIndep" in indep._repr_html_()
